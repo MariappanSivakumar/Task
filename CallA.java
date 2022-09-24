337bee911529046f327e2fade61148a6ebd010a6
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-09-24 07:41:13 +0100

    Create CallA.java

---
+++ CallA.java
@@ -0,0 +1,26 @@
+abstract class Parent
+{
+	public void print()
+	{
+		System.out.println("Parent class is called");
+		println();
+	}
+	abstract void println();
+}
+class Child extends Parent
+{
+	@Override 
+	void println()
+	{
+		System.out.println("Child class is called");
+	}
+}
+class CallA extends Child
+{
+	public static void main(String ar[])
+	{
+		//   >>>>>>>  Second Way Of Solving      <<<
+		Call c=new Call();
+		c.print();
+	}
+}
