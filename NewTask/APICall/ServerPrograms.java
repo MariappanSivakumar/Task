eac171492b9f81b5463d97eb273aa60d66be330f
Mariappan <97144165+MariappanSivakumar@users.noreply.github.com>
2022-11-04 11:55:27 +0530

    Create ServerPrograms.java

---
+++ NewTask/APICall/ServerPrograms.java
@@ -0,0 +1,12 @@
+
+package serverprograms;
+
+public class ServerPrograms {
+
+    public static void main(String[] args) {
+        //HttpURLConnectionExample Call 
+        HttpURLConnectionAPICall h =new HttpURLConnectionAPICall();
+        h.APICall();
+    }
+    
+}
